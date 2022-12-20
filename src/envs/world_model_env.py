79d46c65b74e7363c8d69b373beda4d9a9374162
mitchellgoffpc <mitchellgoffpc@gmail.com>
2022-12-19 17:33:25 -0800

    Move WorldModelEnv inputs/outputs to/from cpu

--- src/envs/world_model_env.py
+++ src/envs/world_model_env.py
@@ -21,9 +21,9 @@ class WorldModelEnv:
 
         import onnx, numpy as np, onnxruntime as ort
         dummy_tokens = torch.zeros(4, 16, dtype=torch.long)
-        dummy_kv = world_model.transformer.generate_empty_keys_values(4, self.world_model.config.max_tokens // 2)
-        dummy_kv._size = self.world_model.config.max_tokens // 2
-        torch.onnx.export(self.world_model, (dummy_tokens, dummy_kv.get()), '/tmp/wm.onnx', opset_version=11,
+        dummy_kv = world_model.transformer.generate_empty_keys_values(4, 20*16)
+        dummy_kv._size = self.world_model.config.max_tokens
+        torch.onnx.export(self.world_model.cpu(), (dummy_tokens, dummy_kv.get().cpu()), '/tmp/wm.onnx', opset_version=11,
           input_names=['tokens', 'past'],
           output_names=['output_seq', 'logits_obs', 'logits_reward', 'logits_done', 'kv'],
           dynamic_axes={
@@ -35,7 +35,7 @@ class WorldModelEnv:
             'logits_done': {0: 'b', 1: 't_done'},
             'kv': {2: 'b', 3: 'nh', 4: 't', 5: 'hs'}})
 
-        self.wm_runner = ort.InferenceSession('/tmp/wm.onnx', None, ['CPUExecutionProvider'])
+        self.wm_runner = ort.InferenceSession('/tmp/wm.onnx', None, ['CUDAExecutionProvider'])
         self.input_names = [x.name for x in self.wm_runner.get_inputs()]
         self.output_names = [x.name for x in self.wm_runner.get_outputs()]
 
@@ -46,13 +46,17 @@ class WorldModelEnv:
 
     @torch.no_grad()
     def reset(self) -> torch.FloatTensor:
+        self.tokenizer = self.tokenizer.cpu()
+        self.world_model = self.world_model.cpu()
         assert self.env is not None
         obs = torchvision.transforms.functional.to_tensor(self.env.reset()).to(self.device).unsqueeze(0)  # (1, C, H, W) in [0., 1.]
         return self.reset_from_initial_observations(obs)
 
     @torch.no_grad()
     def reset_from_initial_observations(self, observations: torch.FloatTensor) -> torch.FloatTensor:
-        obs_tokens = self.tokenizer.encode(observations, should_preprocess=True).tokens    # (B, C, H, W) -> (B, K)
+        self.tokenizer = self.tokenizer.cpu()
+        self.world_model = self.world_model.cpu()
+        obs_tokens = self.tokenizer.encode(observations.cpu(), should_preprocess=True).tokens    # (B, C, H, W) -> (B, K)
         _, num_observations_tokens = obs_tokens.shape
         if self.num_observations_tokens is None:
             self._num_observations_tokens = num_observations_tokens
@@ -67,7 +71,7 @@ class WorldModelEnv:
         n, num_observations_tokens = obs_tokens.shape
         assert num_observations_tokens == self.num_observations_tokens
         self.keys_values_wm = self.world_model.transformer.generate_empty_keys_values(n=n, max_tokens=self.world_model.config.max_tokens)
-        outputs_wm = self.wm_runner.run(None, {'tokens': obs_tokens.numpy(), 'past': self.keys_values_wm.get().numpy()})
+        outputs_wm = self.wm_runner.run(None, {'tokens': obs_tokens.cpu().numpy(), 'past': self.keys_values_wm.get().numpy()})
         outputs_wm = WorldModelOutput(*outputs_wm)
         self.keys_values_wm.update(torch.as_tensor(outputs_wm.keys_values))
 
@@ -79,8 +83,8 @@ class WorldModelEnv:
         if self.keys_values_wm.size + num_passes > self.world_model.config.max_tokens:
             self.refresh_keys_values_with_initial_obs_tokens(self.obs_tokens)
 
-        token = action.clone().detach() if isinstance(action, torch.Tensor) else torch.tensor(action, dtype=torch.long)
-        token = token.reshape(-1, 1).to(self.device)  # (B, 1)
+        token = action.clone().detach().cpu() if isinstance(action, torch.Tensor) else torch.tensor(action, dtype=torch.long)
+        token = token.reshape(-1, 1)  # (B, 1)
 
         output_sequence, obs_tokens = [], []
         for k in range(num_passes):  # assumption that there is only one action token.
@@ -100,8 +104,7 @@ class WorldModelEnv:
 
         output_sequence = torch.cat(output_sequence, dim=1)   # (B, 1 + K, E)
         self.obs_tokens = torch.cat(obs_tokens, dim=1)        # (B, K)
-
-        obs = self.decode_obs_tokens() if should_predict_next_obs else None
+        obs = self.decode_obs_tokens().to(self.device) if should_predict_next_obs else None
         return obs, reward, done, None
 
     @torch.no_grad()
@@ -112,10 +115,10 @@ class WorldModelEnv:
 
     @torch.no_grad()
     def decode_obs_tokens(self) -> List[Image.Image]:
-        embedded_tokens = self.tokenizer.embedding(self.obs_tokens)     # (B, K, E)
+        embedded_tokens = self.tokenizer.embedding(self.obs_tokens.cpu())     # (B, K, E)
         z = rearrange(embedded_tokens, 'b (h w) e -> b e h w', h=int(np.sqrt(self.num_observations_tokens)))
         rec = self.tokenizer.decode(z, should_postprocess=True)         # (B, C, H, W)
-        return torch.clamp(rec, 0, 1)
+        return torch.clamp(rec, 0, 1).to(self.device)
 
     @torch.no_grad()
     def render(self):
